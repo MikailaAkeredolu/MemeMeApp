8ba655c159b098d1e1ae58e92f7b674291427ee1
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2015-03-30 13:25:20 -0400

    UiCoolection and tableviews

---
+++ MemeClass.swift
@@ -0,0 +1,25 @@
+//
+//  MemeClass.swift
+//  MemeMeApp
+//
+//  Created by Mikaila Akeredolu on 3/29/15.
+//  Copyright (c) 2015 MakerOfAppsDotCom. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeClass {
+    
+   // properties and methods
+    var topTextField = ""
+    var bottomTextField = ""
+    var originalImage = UIImage()
+    var memedImage = UIImage()
+    
+    init(textTop:UITextField, textBottom: UITextField, image:UIImage, mImage:UIImage){
+        
+    }
+    
+}
+
