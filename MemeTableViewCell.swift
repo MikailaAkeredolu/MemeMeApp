d8e3c0a55bf1f951c5cb5bf41c100c2baeee6e7c
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2015-04-02 07:10:30 -0400

    working on tableView

---
+++ MemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMeApp
+//
+//  Created by Mikaila Akeredolu on 4/1/15.
+//  Copyright (c) 2015 MakerOfAppsDotCom. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
